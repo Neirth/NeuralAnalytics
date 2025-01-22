d692a16454ea0fdeb98b06b8784120c41e2993e5
Sergio Martínez <7413024+Neirth@users.noreply.github.com>
2025-01-22 13:28:50 +0100

    chore: Added license header

--- packages/neural_analytics_model/src/utils/evaluation.py
+++ packages/neural_analytics_model/src/utils/evaluation.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2025 Sergio Martínez Aznar
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import torch
 from preprocessors.neural_analytics import calculate_r2_from_csv
 from sklearn.metrics import r2_score
